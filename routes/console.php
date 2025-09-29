9dd90281a62bcc17adec077567a13354a497970a
Javier <javier.gamonal.dev@gmail.com>
2025-09-29 10:00:53 -0300

    comentando schedule

--- routes/console.php
+++ routes/console.php
@@ -8,9 +8,9 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('orders:webpay:expire --ttl=30')
-    ->everyFiveMinutes()
-    ->withoutOverlapping()
-    ->onOneServer()          // si tienes varios pods/instancias
-    ->runInBackground();      // no bloquea el scheduler
-    // ->environments(['production']) // opcional
+// Schedule::command('orders:webpay:expire --ttl=30')
+//     ->everyFiveMinutes()
+//     ->withoutOverlapping()
+//     ->onOneServer()          // si tienes varios pods/instancias
+//     ->runInBackground();      // no bloquea el scheduler
+//     // ->environments(['production']) // opcional
